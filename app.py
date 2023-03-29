6cddcb3a8be51f2c942bcc7f1e1031f17d270cf3
AshutoshDas108 <112765623+AshutoshDas108@users.noreply.github.com>
2023-03-29 11:51:35 +0530

    Update app.py
    Overall analysis

--- app.py
+++ app.py
@@ -8,3 +8,45 @@ df = pd.read_csv('startup_cleaned.csv')
 df['date'] = pd.to_datetime(df['date'],errors='coerce')
 df['month'] = df['date'].dt.month
 df['year'] = df['date'].dt.year
+
+
+
+def load_overall_analysis():
+    st.title('Overall Analysis')
+
+    # total invested amount
+    total = round(df['amount'].sum())
+    # max amount infused in a startup
+    max_funding = df.groupby('startup')['amount'].max().sort_values(ascending=False).head(1).values[0]
+    # avg ticket size
+    avg_funding = df.groupby('startup')['amount'].sum().mean()
+    # total funded startups
+    num_startups = df['startup'].nunique()
+
+    col1,col2,col3,col4 = st.columns(4)
+
+    with col1:
+        st.metric('Total',str(total) + ' Cr')
+    with col2:
+        st.metric('Max', str(max_funding) + ' Cr')
+
+    with col3:
+        st.metric('Avg',str(round(avg_funding)) + ' Cr')
+
+    with col4:
+        st.metric('Funded Startups',num_startups)
+
+    st.header('MoM graph')
+    selected_option = st.selectbox('Select Type',['Total','Count'])
+    if selected_option == 'Total':
+        temp_df = df.groupby(['year', 'month'])['amount'].sum().reset_index()
+    else:
+        temp_df = df.groupby(['year', 'month'])['amount'].count().reset_index()
+
+    temp_df['x_axis'] = temp_df['month'].astype('str') + '-' + temp_df['year'].astype('str')
+
+    fig3, ax3 = plt.subplots()
+    ax3.plot(temp_df['x_axis'], temp_df['amount'])
+
+    st.pyplot(fig3)
+
