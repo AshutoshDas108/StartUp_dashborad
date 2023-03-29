ffe74391c7cdc180ffa059bb736357e26e86777e
AshutoshDas108 <112765623+AshutoshDas108@users.noreply.github.com>
2023-03-29 11:50:50 +0530

    Update app.py
    extracting time data

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
+
+df = pd.read_csv('startup_cleaned.csv')
+df['date'] = pd.to_datetime(df['date'],errors='coerce')
+df['month'] = df['date'].dt.month
+df['year'] = df['date'].dt.year
